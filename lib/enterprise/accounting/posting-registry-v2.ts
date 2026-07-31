7465e9103dce94745f954f24863aa1cd3223ce1a
Bandsman942 <jntumba01@outlook.com>
2026-07-31 14:20:17 +0100

    refactor: route temporary registry name to final registry

---
+++ lib/enterprise/accounting/posting-registry-v2.ts
@@ -0,0 +1,2 @@
+export { ENTERPRISE_POSTING_REGISTRY, getPostingBuilder as getPostingBuilderV2 } from "@/lib/enterprise/accounting/posting-registry-final";
+export type { PostingBuilder, PostingDocument, PostingLineDraft } from "@/lib/enterprise/accounting/posting-types";
