e22d1a08cf38f087bf89415bef7a2f3471245732
Bandsman942 <jntumba01@outlook.com>
2026-09-12 21:16:48 +0100

    feat(accounting): route C5 closing operations workspace

---
+++ app/enterprise-modules/FINANCE_CLOSE/operations/page.tsx
@@ -0,0 +1,5 @@
+import { EnterpriseClosingOperationsPage } from "@/components/enterprise/enterprise-closing-operations-page";
+
+export default function Page() {
+  return <EnterpriseClosingOperationsPage />;
+}
